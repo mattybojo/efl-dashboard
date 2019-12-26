b3534cd2d670813407c2fe06540aff027b463758
mattybojo <mattbojorquez@gmail.com>
2019-12-26 14:09:11 -0800

    Sidebar now closes on menu item click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { NbMenuService, NbSidebarService } from '@nebular/theme';
 /**
  * @license
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { MENU_ITEMS } from './pages/pages-menu';
 import { EFL_MENU_ITEMS, EFL_ADMIN_MENU_ITEMS } from './app-menu';
 import { AuthService } from './shared/services/auth.service';
 import { NbMenuItem, NbIconLibraries } from '@nebular/theme';
+import { LayoutService } from './@core/utils';
 
 @Component({
   selector: 'ngx-app',
@@ -23,7 +25,9 @@ export class AppComponent implements OnInit {
 
   menu: NbMenuItem[];
 
-  constructor(private authService: AuthService, private iconLibraries: NbIconLibraries) {
+  constructor(private authService: AuthService, private iconLibraries: NbIconLibraries,
+      private nbMenuService: NbMenuService, private sidebarService: NbSidebarService,
+      private layoutService: LayoutService, private elem: ElementRef) {
     this.iconLibraries.registerFontPack('solid', {packClass: 'fas', iconClassPrefix: 'fa'});
     this.iconLibraries.registerFontPack('regular', {packClass: 'far', iconClassPrefix: 'fa'});
     this.iconLibraries.registerFontPack('light', {packClass: 'fal', iconClassPrefix: 'fa'});
@@ -32,10 +36,17 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const self = this;
+    this.nbMenuService.onItemClick().subscribe(() => {
+      const elements = self.elem.nativeElement.querySelectorAll('.menu-sidebar.expanded');
+      if (elements.length > 0) {
+        self.toggleSidebar();
+      }
+    });
     this.authService.decryptUserData();
     this.menu = EFL_MENU_ITEMS;
     this.authService.getUserDataObservable().subscribe(user => {
-      let temp: NbMenuItem[] = new Array<NbMenuItem>();
+      const temp: NbMenuItem[] = new Array<NbMenuItem>();
       if (user) {
         if (user.id === 'RAWeDzKz6ABgEnWsONnI') {
           this.menu = temp.concat(EFL_MENU_ITEMS, EFL_ADMIN_MENU_ITEMS, MENU_ITEMS);
@@ -46,5 +57,12 @@ export class AppComponent implements OnInit {
         this.menu = EFL_MENU_ITEMS;
       }
     });
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    this.layoutService.changeLayoutSize();
+
+    return false;
   }
 }
