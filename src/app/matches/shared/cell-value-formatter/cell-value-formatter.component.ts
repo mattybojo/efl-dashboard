76024ced27f1e25ee7d501df81eb9cab23062f89
mattybojo <mattbojorquez@gmail.com>
2019-12-27 13:02:54 -0800

    Cleaned up CellValueFormatter component

--- src/app/matches/shared/cell-value-formatter/cell-value-formatter.component.ts
+++ src/app/matches/shared/cell-value-formatter/cell-value-formatter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faCuttlefish, faAdn } from '@fortawesome/free-brands-svg-icons';
 import { faCrown, faFutbol, faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,7 +7,7 @@ import { faCrown, faFutbol, faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './cell-value-formatter.component.html',
   styleUrls: ['./cell-value-formatter.component.scss']
 })
-export class CellValueFormatterComponent implements OnInit {
+export class CellValueFormatterComponent {
 
   @Input() value: string;
   @Input() rowData: any;
@@ -21,9 +21,5 @@ export class CellValueFormatterComponent implements OnInit {
   faCuttlefish = faCuttlefish;
   faAdn = faAdn;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
